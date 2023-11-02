6cce9312c91d6cc88810dbad884b5fc9501a4961
youssefDev <youssef.apps@gmail.com>
2023-11-02 15:48:32 +0100

    first commit3

---
+++ info.js
@@ -0,0 +1,12 @@
+
+
+// build time     
+// request time
+// run time
+
+
+// 2 types of pre-rendering: 
+     //Static Generation :enerates the HTML at build time :  with data and without data using getStaticProps
+     //and Server-side Rendering : generates the HTML on each request.
+
+     //OpenAI key :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
